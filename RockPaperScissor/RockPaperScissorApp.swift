f483e08cf3166a1e46c2112e2b082b51aa975b8f
joy jain <joyshah204@gmial.com>
2023-12-03 19:21:56 +0530

    Initial Commit

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by JOY JAIN on 03/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
